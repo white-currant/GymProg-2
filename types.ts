836602004b7c6469cbcd21c4b231ad56532cb322
white-currant <yulion_primary@proton.me>
2026-01-13 21:55:19 +0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,30 +1,30 @@
 
-export type WorkoutSet = {
+export interface WorkoutSet {
   reps: number;
   weight: number;
-};
+}
 
-export type Exercise = {
+export interface Exercise {
   id: string;
   name: string;
   sets: WorkoutSet[];
-};
+}
 
 export type WorkoutType = 'A' | 'B';
 
-export type Workout = {
+export interface Workout {
   id: string;
   date: string;
   type: WorkoutType;
   exercises: Exercise[];
   userWeight?: number;
   duration?: number;
-};
+}
 
-export type UserProfile = {
+export interface UserProfile {
   name: string;
   email: string;
   picture: string;
-};
+}
 
 export type Tab = 'dashboard' | 'history' | 'analytics' | 'add' | 'settings';
